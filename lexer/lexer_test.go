e1f51d7d3bb7facc49977d50b97c45cdab445785
Adrian Brady <ajbrady999@protonmail.com>
2024-06-08 16:36:53 -0500

    add space indentation test

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -103,3 +103,57 @@ NOT R1,R1
 		}
 	}
 }
+
+func TestSpaceIndentation(t *testing.T) {
+	input := `LABEL:
+  R5,R5,R5;
+  ADD R5,R5,#1
+NOT R1,R1
+`
+
+	tests := []struct {
+		expectedType    token.TokenType
+		expectedLiteral string
+	}{
+		{token.IDENT, "LABEL"},
+		{token.COLON, ":"},
+
+		{token.INDENT, "INDENT"},
+
+		{token.REGISTER, "R5"},
+		{token.COMMA, ","},
+		{token.REGISTER, "R5"},
+		{token.COMMA, ","},
+		{token.REGISTER, "R5"},
+		{token.SEMICOLON, ";"},
+
+		{token.ADD, "ADD"},
+		{token.REGISTER, "R5"},
+		{token.COMMA, ","},
+		{token.REGISTER, "R5"},
+		{token.COMMA, ","},
+		{token.HASH, "#"},
+		{token.INT, "1"},
+
+		{token.DEDENT, "DEDENT"},
+
+		{token.NOT, "NOT"},
+		{token.REGISTER, "R1"},
+		{token.COMMA, ","},
+		{token.REGISTER, "R1"},
+	}
+
+	l := New(input)
+
+	for i, tt := range tests {
+		tok := l.NextToken()
+
+		if tok.Type != tt.expectedType {
+			t.Errorf("tests[%d] - tokentype wrong. expected=%q, got=%q", i, tt.expectedType, tok.Type)
+		}
+
+		if tok.Literal != tt.expectedLiteral {
+			t.Errorf("tests[%d] - literal wrong. expected=%q, got=%q", i, tt.expectedLiteral, tok.Literal)
+		}
+	}
+}
